f4047ac378501e9f2530c0a4670f8bc632109398
vishwa <saivishwa.gaddam.com>
2023-08-02 15:14:23 -0700

    azure ci cd deployment 1

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 
+
+
 @app.route('/', methods=['GET', 'POST']) # To render Homepage
 def home_page():
     return render_template("index.html")
